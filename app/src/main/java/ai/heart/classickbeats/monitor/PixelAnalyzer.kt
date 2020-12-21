c9f55252bc331a2eb4c5efda6897f23f211b04a2
vipgupta <vipgupta84@gmail.com>
2020-12-21 23:28:14 +0530

    Added fn yuvtoARGBvip to PixelAnalyzer

--- app/src/main/java/ai/heart/classickbeats/monitor/PixelAnalyzer.kt
+++ app/src/main/java/ai/heart/classickbeats/monitor/PixelAnalyzer.kt
@@ -1,6 +1,10 @@
 package ai.heart.classickbeats.monitor
 
+import android.R.attr.x
+import android.R.attr.y
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.SystemClock
 import android.renderscript.*
 import android.text.format.DateUtils
@@ -29,7 +33,7 @@ class PixelAnalyzer constructor(private val context: Context) : ImageAnalysis.An
     }
 
     override fun analyze(image: ImageProxy) {
-        Timber.i("cameraOutput: new Image received")
+        // Timber.i("cameraOutput: new Image received")
         val output = processImage(image)
         image.close()
     }
@@ -37,15 +41,39 @@ class PixelAnalyzer constructor(private val context: Context) : ImageAnalysis.An
     private fun processImage(image: ImageProxy) {
         val argbArray = yuv420ToARGB(image, context)
         // Process the output array here
-
+        var h = image.height;
+        var w = image.width;
+        var offset: Float = 0F
+        // Timber.i("Height: $h$, Width: $w$")
         displayCounter()
+        // val arr_size = argbArray.count()
+//        val a_sum = argbArray.sliceArray(0..(h*w-1))
+//        val a_mean = a_sum.average() + offset
+//        val r_sum = argbArray.sliceArray((h*w)..(2*h*w-1))
+//        val r_mean = r_sum.average() + offset
+//        val g_sum = argbArray.sliceArray((2*h*w)..(3*h*w-1))
+//        val g_mean = g_sum.average() + offset
+//        val b_sum = argbArray.sliceArray((3*h*w)..(4*h*w-1))
+//        val b_mean = b_sum.average() + offset
+//        //val n120 = argbArray.get(0)
+//        Timber.i("A mean: $a_mean, R mean: $r_mean, G mean: $g_mean, B mean: $b_mean")
+        // Timber.i("120th value: $n120$")
+
+        val bitmap = yuvtoARGBvip(image, context)
+        val colour = bitmap.getPixel(10, 10)
+
+        val red: Int = Color.red(colour)
+        val blue: Int = Color.blue(colour)
+        val green: Int = Color.green(colour)
+        val alpha: Int = Color.alpha(colour)
+        Timber.i("A: $alpha, R: $red, G: $green, B: $blue")
     }
 
     private fun displayCounter() {
         val currentTime = SystemClock.elapsedRealtime()
         previousSecond = currentSecond
         currentSecond = currentTime / DateUtils.SECOND_IN_MILLIS
-        Timber.i("$previousSecond, $currentSecond")
+        // Timber.i("$previousSecond, $currentSecond")
         if (previousSecond == currentSecond) {
             counter++
         } else {
@@ -81,6 +109,27 @@ class PixelAnalyzer constructor(private val context: Context) : ImageAnalysis.An
 
         outputAllocation?.copyTo(outputArray)
         return outputArray
+    }
+
+    private fun yuvtoARGBvip(image: ImageProxy, context: Context): Bitmap {
+        val byteArray = yuv420ToByteArray(image)
+        val rs = RenderScript.create(context)
+        val yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs))
+
+        val yuvType = Type.Builder(rs, Element.U8(rs)).setX(byteArray.size)
+        val inData = Allocation.createTyped(rs, yuvType.create(), Allocation.USAGE_SCRIPT)
+
+        val rgbaType = Type.Builder(rs, Element.RGBA_8888(rs)).setX(image.width).setY(image.height)
+        val outData = Allocation.createTyped(rs, rgbaType.create(), Allocation.USAGE_SCRIPT)
+
+        inData.copyFrom(byteArray)
+
+        yuvToRgbIntrinsic.setInput(inData)
+        yuvToRgbIntrinsic.forEach(outData)
+
+        val bitmap = Bitmap.createBitmap(image.width, image.height, Bitmap.Config.ARGB_8888)
+        outData.copyTo(bitmap)
+        return bitmap
     }
 
     private fun yuv420ToByteArray(image: ImageProxy): ByteArray {
