491f079068619fe315097f7d091c59f9a33df05b
Ritesh Kumar <riteshkumar4302@gmail.com>
2021-01-30 00:56:36 +0530

    primary changes for cmake

--- app/src/main/java/ai/heart/classickbeats/MainActivity.kt
+++ app/src/main/java/ai/heart/classickbeats/MainActivity.kt
@@ -8,9 +8,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        System.loadLibrary("native-lib")
     }
 
     fun hideSystemUI() {
