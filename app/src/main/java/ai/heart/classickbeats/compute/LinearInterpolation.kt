5c7ffb18be7f28c80eefdbdfa2b0e4c22ba801c7
Ritesh Kumar <riteshkumar4302@gmail.com>
2021-02-13 14:23:04 +0530

    adds apache common math library

--- app/src/main/java/ai/heart/classickbeats/compute/LinearInterpolation.kt
+++ app/src/main/java/ai/heart/classickbeats/compute/LinearInterpolation.kt
@@ -1,5 +1,8 @@
 package ai.heart.classickbeats.compute
 
+import org.apache.commons.math3.analysis.interpolation.AkimaSplineInterpolator
+
 class LinearInterp{
 
+    val a = AkimaSplineInterpolator()
 }
