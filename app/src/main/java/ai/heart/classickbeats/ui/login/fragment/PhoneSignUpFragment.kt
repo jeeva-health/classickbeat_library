529b16c857a1f1781f95bc0ae2720356205c3314
Ritesh Kumar <riteshkumar4302@gmail.com>
2021-04-16 01:18:40 +0530

    adds phone number screen

---
+++ app/src/main/java/ai/heart/classickbeats/ui/login/fragment/PhoneSignUpFragment.kt
@@ -0,0 +1,16 @@
+package ai.heart.classickbeats.ui.login.fragment
+
+import ai.heart.classickbeats.R
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+
+class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
