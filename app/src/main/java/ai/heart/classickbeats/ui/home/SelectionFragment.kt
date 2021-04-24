bc671caa4268057743e1de264ea9511435a0fa18
Ritesh Kumar <riteshkumar4302@gmail.com>
2021-04-24 08:05:39 +0530

    fixes permission

--- app/src/main/java/ai/heart/classickbeats/ui/home/SelectionFragment.kt
+++ app/src/main/java/ai/heart/classickbeats/ui/home/SelectionFragment.kt
@@ -6,9 +6,13 @@ import ai.heart.classickbeats.domain.TestType
 import ai.heart.classickbeats.ui.login.LoginViewModel
 import ai.heart.classickbeats.utils.setSafeOnClickListener
 import ai.heart.classickbeats.utils.viewBinding
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
@@ -31,6 +35,47 @@ class SelectionFragment : Fragment(R.layout.fragment_selection) {
 
     private lateinit var logoutButton: AppCompatButton
 
+    private val requiredPermissions = listOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    private val permissionToRequest = mutableListOf<String>()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissionResult ->
+
+            requiredPermissions.forEach { permission ->
+                if (permissionResult[permission] == true) {
+                    permissionToRequest.remove(permission)
+                }
+            }
+            if (ifAllMustPermissionsAreGranted()) {
+
+            }
+        }
+
+    private fun ifAllMustPermissionsAreGranted() = permissionToRequest.isEmpty()
+
+    private fun requestForPermissions() {
+        if (ifAllMustPermissionsAreGranted()) {
+
+        } else {
+            requestPermissionLauncher.launch(permissionToRequest.toTypedArray())
+        }
+    }
+
+    private fun checkPermission(permission: String) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionToRequest.add(permission)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -52,6 +97,11 @@ class SelectionFragment : Fragment(R.layout.fragment_selection) {
             logInViewModel.logoutUser()
             requireActivity().finish()
         }
+
+        checkPermission(Manifest.permission.CAMERA)
+        checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        requestForPermissions()
     }
 
     private fun navigateToScanFragment() {
