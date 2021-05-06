c33fc97b39754d9dd7b6350f72366923d3f49404
Ritesh Kumar <riteshkumar4302@gmail.com>
2021-05-06 10:46:43 +0530

    signup changes and gradle updates

---
+++ app/src/main/java/ai/heart/classickbeats/ui/login/fragment/GoogleSignUpFragment.kt
@@ -0,0 +1,103 @@
+package ai.heart.classickbeats.ui.login.fragment
+
+import ai.heart.classickbeats.R
+import ai.heart.classickbeats.ui.login.FirebaseGoogleSignIn
+import ai.heart.classickbeats.ui.login.LoginViewModel
+import ai.heart.classickbeats.utils.EventObserver
+import ai.heart.classickbeats.utils.showLongToast
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
+
+
+class GoogleSignUpFragment : Fragment(R.layout.fragment_google_sign_up) {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var navController: NavController
+
+    private val logInViewModel by activityViewModels<LoginViewModel>()
+
+    private val googleSigInLauncher = registerForActivityResult(FirebaseGoogleSignIn()) {
+        if (it != null) {
+            Timber.d("firebaseAuthWithGoogle: ${it.id}")
+            firebaseAuthWithGoogle(it.idToken!!)
+        } else {
+            Timber.e("Google sign in failed")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = findNavController()
+
+        googleSigInLauncher.launch(googleSignInClient)
+
+        logInViewModel.apply {
+            loginState.observe(viewLifecycleOwner, EventObserver { isUserLoggedIn ->
+                if (isUserLoggedIn) {
+                    if (isUserRegistered) {
+                        navigateToHomeFragment()
+                    } else {
+                        navigateToRegisterFragment()
+                    }
+                }
+            })
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Timber.d("signInWithCredential:success")
+                    val user = auth.currentUser
+                    val firebaseToken = user?.getIdToken(false)?.result?.token
+                    logInViewModel.loginUser(firebaseToken!!)
+                } else {
+                    Timber.w("signInWithCredential:failure ${task.exception}")
+                    showLongToast("Google login failed")
+                    navController.navigateUp()
+                }
+            }
+    }
+
+    private fun navigateToHomeFragment() {
+        val action = GoogleSignUpFragmentDirections.actionGoogleSignUpFragmentToNavHome()
+        navController.navigate(action)
+    }
+
+    private fun navigateToRegisterFragment() {
+        val action =
+            GoogleSignUpFragmentDirections.actionGoogleSignUpFragmentToPersonalDetailsFragment()
+        navController.navigate(action)
+    }
+}
--- app/src/main/java/ai/heart/classickbeats/ui/login/fragment/PhoneSignUpFragment.kt
+++ app/src/main/java/ai/heart/classickbeats/ui/login/fragment/PhoneSignUpFragment.kt
@@ -5,9 +5,12 @@ import ai.heart.classickbeats.databinding.FragmentPhoneSignUpBinding
 import ai.heart.classickbeats.ui.login.LoginViewModel
 import ai.heart.classickbeats.utils.*
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
@@ -24,6 +27,13 @@ class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
 
     private val logInViewModel by activityViewModels<LoginViewModel>()
 
+    private var timer: CountDownTimer? = null
+
+    var isTimerRunning: Boolean = false
+        private set
+
+    val timerProgress = MutableLiveData(Event(60))
+
     private lateinit var auth: FirebaseAuth
     private var storedVerificationId: String? = ""
     private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
@@ -36,12 +46,14 @@ class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
 
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                endTimer()
                 Timber.d("onVerificationCompleted:$credential")
                 signInWithPhoneAuthCredential(credential)
             }
 
             override fun onVerificationFailed(e: FirebaseException) {
                 Timber.w("onVerificationFailed: $e")
+                endTimer()
                 showShortToast("Error occurred!!!")
                 if (e is FirebaseAuthInvalidCredentialsException) {
                     // Invalid request
@@ -57,6 +69,7 @@ class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
                 Timber.d("onCodeSent:$verificationId")
                 storedVerificationId = verificationId
                 resendToken = token
+                startTimer()
             }
         }
     }
@@ -77,11 +90,21 @@ class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
             it.visibility = View.GONE
             binding.verifyOtpBtn.visibility = View.VISIBLE
             binding.otpLayout.visibility = View.VISIBLE
+            binding.resendTxt.visibility = View.VISIBLE
+            binding.resend.visibility = View.VISIBLE
+            binding.numberLayout.isEnabled = false
+            binding.otpLayout.requestFocus()
         }
 
         binding.verifyOtpBtn.setSafeOnClickListener {
             val otp = binding.otpLayout.editText?.text?.toString()!!
             verifyPhoneNumberWithCode(storedVerificationId, otp)
+        }
+
+        binding.resend.setSafeOnClickListener {
+            val phoneNumber = "+91" + binding.numberLayout.editText?.text?.toString()!!
+            resendVerificationCode(phoneNumber, resendToken)
+            binding.otpLayout.requestFocus()
         }
 
         logInViewModel.apply {
@@ -95,6 +118,10 @@ class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
                 }
             })
         }
+
+        timerProgress.observe(viewLifecycleOwner, EventObserver {
+            binding.resendTxt.text = "Didn't receive the OTP? Retry in $it seconds."
+        })
     }
 
     private fun startPhoneNumberVerification(phoneNumber: String) {
@@ -129,7 +156,7 @@ class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
 
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
         auth.signInWithCredential(credential)
-            .addOnCompleteListener { task ->
+            .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
                     Timber.d("signInWithCredential:success")
                     val user = task.result?.user
@@ -155,5 +182,27 @@ class PhoneSignUpFragment : Fragment(R.layout.fragment_phone_sign_up) {
     private fun navigateToHomeFragment() {
         val action = PhoneSignUpFragmentDirections.actionPhoneSignUpFragmentToNavHome()
         findNavController().navigate(action)
+    }
+
+    fun startTimer(timeLeftInMillis: Long = 60 * DateUtils.SECOND_IN_MILLIS) {
+        timer?.cancel()
+        timer = object : CountDownTimer(timeLeftInMillis, TimeUnit.SECONDS.toMillis(1)) {
+            override fun onFinish() {
+                isTimerRunning = false
+                timerProgress.postValue(Event(0))
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                timerProgress.postValue(Event((millisUntilFinished / DateUtils.SECOND_IN_MILLIS).toInt()))
+                binding.resend.isEnabled = millisUntilFinished <= 1000
+            }
+        }
+        timer?.start()
+        isTimerRunning = true
+    }
+
+    fun endTimer() {
+        timer?.cancel()
+        timer = null
     }
 }
