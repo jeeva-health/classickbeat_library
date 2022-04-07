fd78b1ed823798c85de72e367cc12a2121be3ad6
cr-velox-git <48889698+cr-velox-git@users.noreply.github.com>
2022-04-07 09:56:43 +0530

    working on  my health fragment ui

---
+++ app/src/main/java/ai/heart/classickbeats/ui/ppg/fragment/my_health/SaveWeightFragment.kt
@@ -0,0 +1,16 @@
+package ai.heart.classickbeats.ui.ppg.fragment.my_health
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ai.heart.classickbeats.R
+
+
+class SaveWeightFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/ai/heart/classickbeats/ui/ppg/fragment/my_health/WaterInTakeFragment.kt
@@ -0,0 +1,23 @@
+package ai.heart.classickbeats.ui.ppg.fragment.my_health
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ai.heart.classickbeats.R
+
+
+class WaterInTakeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
