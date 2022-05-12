ae4aab779fe2937ac27cb457426c332a0157f519
RajeshIsu <rajesh.k@iserveu.in>
2022-05-13 00:13:48 +0530

    starting with compose

--- app/src/main/java/ai/heart/classickbeats/ui/logging/BloodPressureFragment.kt
+++ app/src/main/java/ai/heart/classickbeats/ui/logging/BloodPressureFragment.kt
@@ -1,23 +1,31 @@
 package ai.heart.classickbeats.ui.logging
 
+import ai.heart.classickbeats.R
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 
 class BloodPressureFragment : Fragment() {
@@ -30,24 +38,90 @@ class BloodPressureFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             // Dispose of the Composition when the view's LifecycleOwner
             // is destroyed
-            // setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                mainCompose()
+                MainCompose()
             }
         }
     }
 
 
+    @SuppressLint("ResourceType")
     @Composable
     @Preview
-    fun mainCompose() {
-        Column(
+    fun MainCompose() {
+        val brush = Brush.verticalGradient(
+            colors = listOf(Color(0xFFE1F2F6), Color(0xFFD3062A)),
+
+            )
+        Surface(
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
-                .backg
+                .background(brush = brush)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+            ) {
+                ToolBarLayout()
+                MaterialTheme() {
+                    Text("first compose")
+
+                }
+            }
+
+
         }
 
     }
 
+    @Composable
+    fun ToolBarLayout() {
+        Row(
+            modifier = Modifier
+                .height(56.dp)
+                .fillMaxWidth()
+                .background(Color.White),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .width(24.dp)
+                    .height(24.dp)
+                    .clickable { onBackPressed() },
+                content = {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_baseline_arrow_back),
+                        contentDescription = null
+                    )
+                }
+            )
+//            {
+//                Image(
+//                    painter = painterResource(id = R.drawable.ic_baseline_arrow_back),
+//                    contentDescription = null
+//                )
+//            }
+            Text(
+                // modifier = Modifier.padding(16.dp),
+                text = "Blood Pressure",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.SansSerif,
+            )
+        }
+    }
+
+
+    //..........................FUNCTIONS...........................//
+    private fun onBackPressed() {
+        TODO("Not yet implemented")
+    }
+
+
 }
+
